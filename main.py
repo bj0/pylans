a5893a7057e4c8fa79883584edf2d50ee1ee18d5
Brian Parma <execrable@gmail.com>
2011-12-29 15:11:34 -0700

    add some extra options for logfile and starting (not all usable yet)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from vpn import settings
 if platform.system() == 'Windows':
     __file__ = sys.argv[0]
 
-# Find out the location of exaile's working directory, and insert it to sys.path
+# Find out the location of pylan's working directory, and insert it to sys.path
 basedir = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(os.path.join(basedir, "main.py")):
     cwd = os.getcwd()
@@ -33,16 +33,34 @@ def main():
     root_check()
 
     op = optparse.OptionParser()
-    op.add_option('--gui', action='store_true', default=False, help='Run with gui (default uses cli')
+    op.add_option('--gui', action='store_true', default=False, help='Run with gui (default uses cli)')
+    op.add_option('--daemon', '-D', action='store_true', default=False, help='Run in daemon mode (no input)')
+    op.add_option('--pbi', action='store_true', default=False, help='Run in daemon mode with perspective broker interface')
+    op.add_option('--log-file', '-l', help='Specify log file')
     (ops, args) = op.parse_args()
     
-    if ops.gui:
+    # needs testing TODO
+    if ops.log_file is not None:
+        import logging
+        logging.basicConfig(filename=ops.log_file)
+    
+    if ops.gui: #needs updating TODO
         import gui.main
         gui.main.main()
+        
+    elif ops.daemon: #needs testing TODO
+        from vpn.interface import Interface
+        iface = Interface()
+        iface.start_all_networks()
+        reactor.run()
+        
+    elif ops.pbi: #needs implimenting and testing TODO
+        import pbi
+        pbi.main()
+        
     else:
         import cli.prompt
         cli.prompt.main()
-
 
 
 if __name__ == '__main__':
