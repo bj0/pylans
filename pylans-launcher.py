a2d2ca89616c90ad5ed38e5bcd621f821de04cc1
Brian Parma <execrable@gmail.com>
2012-01-09 15:33:19 -0700

    add a launcher for windows usahs

---
+++ pylans-launcher.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 
+
+#/home/bjp/bin/pypy-c -c "from vpn import settings, prompt; prompt.main()"
+#python -c "import pylans; pylans.main()"
+#python ./main.py
+
+if __name__ == '__main__':
+    import pylans
+    pylans.main()
