1ac872c244f154f12430e0aaf00262a2a21b2e73
Brian Parma <execrable@gmail.com>
2012-01-24 18:34:34 -0700

    remove path hack add logging filter and launch option relative imports

--- pylans/__init__.py
+++ pylans/__init__.py
@@ -4,21 +4,24 @@ import platform
 import sys
 import os
 import optparse
+import logging
 
 # make sure settings file is here
-import settings
+from . import settings
 
 # py2exe TODO: fix this, it's breaking the path in windows
 #if platform.system() == 'Windows':
 #    __file__ = sys.argv[0]
 
 # Find out the location of pylan's working directory, and insert it to sys.path
-basedir = os.path.dirname(os.path.realpath(__file__))
-if not os.path.exists(os.path.join(basedir, "settings.py")):
-    cwd = os.getcwd()
-    if os.path.exists(os.path.join(cwd, "settings.py")):
-        basedir = cwd
-sys.path.insert(0, basedir)
+#basedir = os.path.dirname(os.path.realpath(__file__))
+#if not os.path.exists(os.path.join(basedir, "settings.py")):
+#    cwd = os.getcwd()
+#    if os.path.exists(os.path.join(cwd, "settings.py")):
+#        basedir = cwd
+#sys.path.insert(0, basedir)
+
+settings.FILTER = None
 
 def root_check():
     try:
@@ -37,12 +40,46 @@ def main():
     op.add_option('--daemon', '-D', action='store_true', default=False, help='Run in daemon mode (no input)')
     op.add_option('--pbi', action='store_true', default=False, help='Run in daemon mode with perspective broker interface')
     op.add_option('--log-file', '-l', help='Specify log file')
+    op.add_option('--filter', '-F', type="string", help='filter log with')
     (ops, args) = op.parse_args()
     
+    if ops.filter is not None:
+        settings.FILTER = ops.filter
+    #exaile magic
+    class FilterLogger(logging.Logger):
+        class Filter(logging.Filter):
+            def filter(self, record):
+                if settings.FILTER is None:
+                    return True
+                msg = record.getMessage()
+                string = settings.FILTER
+                return (
+                    string in msg
+                    or string in record.name
+                    or string in record.levelname
+                    or string in record.funcName )
+                
+
+        level = logging.NOTSET
+
+        def __init__(self, name):
+            logging.Logger.__init__(self, name)
+
+            log_filter = self.Filter(name)
+#                log_filter.module = FilterLogger.module
+            log_filter.level = FilterLogger.level
+            self.addFilter(log_filter)
+
+#       FilterLogger.module = self.options.ModuleFilter
+    logging.setLoggerClass(FilterLogger)
+
+    logging.basicConfig(level=settings.get_option('settings/loglevel', 40),
+        format='%(asctime)s:[%(levelname)-10s]>%(name)s:<%(funcName)s>::=> \"%(message)s\"')
+
     # needs testing TODO
     if ops.log_file is not None:
-        import logging
         logging.basicConfig(filename=ops.log_file)
+    
     
     if ops.gui: #needs updating TODO
         import gui.main
@@ -52,7 +89,8 @@ def main():
         from interface import Interface
         from twisted.internet import reactor
         iface = Interface()
-        iface.start_all_networks()
+        reactor.callLater(iface.start_all_networks)
+#        iface.start_all_networks()
         reactor.run()
         
     elif ops.pbi: #needs implimenting and testing TODO
