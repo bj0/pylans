af6b6b004460d78fcb82a9b77a2e4ca2284a761c
Brian Parma <execrable@gmail.com>
2012-03-17 18:55:11 -0700

    change the way the event stuff works with weakrefs: normal Event objects hold strong references, the EventManager holds weakrefs

--- pylans/util/event.py
+++ pylans/util/event.py
@@ -17,24 +17,38 @@
 # event.py
 from __future__ import absolute_import
 import logging
-from . import get_weakref_proxy
+from . import get_weakref_proxy, get_weakref
 
 logger = logging.getLogger(__name__)
 
-class Event:
+class Event(object):
+    '''
+        An event object holds a list of callback functions that get called
+        in the order they are added when fire() is called.  Return values are
+        discarded.  C#-like += and -= syntax supported.
+        
+        Strong references to functions are held, so bound instance methods
+        will keep an object from being garbage collected unless removed.
+        This means that closures and lambdas are also valid.
+        
+    '''
     def __init__(self):
-        self.handlers = set()
+        self.handlers = []
 
     def handle(self, handler):
-        '''Stores weakrefs to bound methods to allow gc'''
-        self.handlers.add(get_weakref_proxy(handler))
+        if handler not in self.handlers:
+            self.handlers.append(handler)
+        else:
+            logger.warning('adding same handler multiple times')
+            
         return self
 
     def unhandle(self, handler):
         try:
-            self.handlers.remove(get_weakref_proxy(handler))
+            self.handlers.remove(handler)
         except IndexError:
-            raise ValueError("Handler is not handling this event, so cannot unhandle it.")
+            raise ValueError("Handler is not handling this event, "
+                             + "so cannot unhandle it.")
         return self
 
     def fire(self, *args, **kargs):
@@ -49,35 +63,69 @@ class Event:
     __call__ = fire
     __len__  = getHandlerCount
 
-class EventManager(object):
+class Callback(object):
+    def __init__(self, function):
+        self.valid = True
+        self.wfunction = get_weakref(function, self.vanished)
+        
+    def __call__(self, *args, **kwargs):
+        f = self.wfunction()
+        if f is None:
+            return None
+        return f(*args, **kwargs)
+        
+    def vanished(self, ref):
+        self.valid = False        
+    
 
+class EventManager(object):
+    '''
+        EventManager manages events and handlers.
+        
+        Handlers are stored as weakrefs, so anything added with register_handler 
+        will not increase the refcount.  This means that objects can be freed
+        after registering a callback (it will be skipped).  This prevents 
+        using lambdas or closures unless external references to them are kept.
+    '''
     def __init__(self):
         self._handlers = {}
 
     def register_handler(self, type, obj, handler):
         _id = id(obj)
 
-        if type not in self._handlers:
-            self._handlers[type] = {}
-            self._handlers[type][id(None)] = Event()
-
-        if _id not in self._handlers[type]:
-            self._handlers[type][_id] = Event()
-
-        self._handlers[type][_id] += handler
-
+        objs = self._handlers.setdefault(type, {})
+        event = objs.setdefault(_id, Event())
+        
+        event += Callback(handler)
+        
     def unregister_handler(self, type, obj, handler):
         self._handlers[type][id(obj)] -= handler
 
     def emit(self, type, obj, *args, **kwargs):
         if type in self._handlers:
-            if obj is not None and \
-             id(obj) in self._handlers[type]:
-                self._handlers[type][id(obj)](obj, *args, **kwargs)
+        
+            def _do_fire(event):
+                if event is not None:
+                    # remove stale weakrefs
+                    for cb in event.handlers:
+                        if not cb.valid:
+                            handlers -= cb
+                    
+                    event.fire(obj, *args, **kwargs)
 
-            self._handlers[type][id(None)](obj, *args, **kwargs)
-            logger.info('event {0} emitted to {1} handlers'.format(type, len(self._handlers[type])))
+                    logger.info('event {0} emitted to {1} handlers'
+                                .format(type, len(event)))
 
+            # fire to object specific handlers
+            event = self._handlers.get(type, {}).get(id(obj), None)
+            _do_fire(event)
+            # fire non-specific handlers
+            if obj is not None:
+                event = self._handlers.get(type, {}).get(id(None), None)
+                _do_fire(event)
+
+            
+                
 MANAGER = EventManager()
 register_handler = MANAGER.register_handler
 unregister_handler = MANAGER.unregister_handler
