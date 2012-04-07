1702a60a1fd30fe51b43774bde196a1f72fb5f3c
Brian Parma <execrable@gmail.com>
2012-04-07 10:07:01 -0700

    move logging stuff to its own module

--- pylans/__init__.py
+++ pylans/__init__.py
@@ -4,27 +4,20 @@ import platform
 import sys
 import os
 import optparse
-import logging
 import re
+import logging
 
+# to get initial logging setup
+from . import plogging
 # make sure settings file is here
 from . import settings
+
+logger = logging.getLogger(__name__)
 
 # py2exe TODO: fix this, it's breaking the path in windows
 #if platform.system() == 'Windows':
 #    __file__ = sys.argv[0]
 
-# Find out the location of pylan's working directory, and insert it to sys.path
-#basedir = os.path.dirname(os.path.realpath(__file__))
-#if not os.path.exists(os.path.join(basedir, "settings.py")):
-#    cwd = os.getcwd()
-#    if os.path.exists(os.path.join(cwd, "settings.py")):
-#        basedir = cwd
-#sys.path.insert(0, basedir)
-
-# non-persistant global settings
-settings.FILTER = []
-settings.IGNORE = []
 settings.is_admin = True
 settings.tap_access = True
 
@@ -38,12 +31,12 @@ def root_check():
         settings.is_admin = shell.IsUserAnAdmin()
         return settings.is_admin
     else:
-        sys.stderr.write('Warning: unknown or unsupported OS\n')
+        logger.warning('Warning: unknown or unsupported OS\n')
 
 
 def main():
     if not root_check():
-        sys.stderr.write('Warning: not running as root, probably wont be able to access tun/tap device or control the adapters...\n')
+        logger.warning('Warning: not running as root, probably wont be able to access tun/tap device or control the adapters...\n')
 
 
     op = optparse.OptionParser()
@@ -72,54 +65,12 @@ def main():
     if ops.ignore is not None:
         settings.IGNORE += [re.compile(s) for s in ops.ignore]
         
-    #exaile's (modified) magic
-    class FilterLogger(logging.Logger):
-        '''A simple logger class that supports filtering'''
-        class Filter(logging.Filter):
-            def filter(self, record):
-                msg = record.getMessage()
-                def check(s):
-                    '''function for checking if s is in record'''
-                    return s.search(''.join((msg,
-                                        record.name,
-                                        record.levelname,
-                                        record.funcName))) is not None
-                        
-                # first show only stuff that passes filter
-                if settings.FILTER:
-                    for s in settings.FILTER:
-                        if check(s):
-                            break
-                    else:
-                        return False
-                
-                # second, ignore stuff that passed filter if it's in ignore
-                if settings.IGNORE:
-                    for s in settings.IGNORE:
-                        if check(s):
-                            return False
-                
-                return True
-                
 
-        level = logging.NOTSET
-
-        def __init__(self, name):
-            logging.Logger.__init__(self, name)
-
-            log_filter = self.Filter(name)
-            log_filter.level = FilterLogger.level
-            self.addFilter(log_filter)
-
-    short_format = '[%(levelname)-10s]: \"%(message)s\"'
-    long_format = '%(asctime)s:[%(levelname)-10s]>%(name)s:<%(funcName)s>::=> \"%(message)s\"'
     
-    fmt = short_format if ops.short else long_format
-
-    logging.setLoggerClass(FilterLogger)
-    logging.basicConfig(level=settings.get_option('settings/loglevel', 40),
-        format=fmt)
-
+    if ops.short:
+        # switch to short formatting
+        plogging.short()
+        
     if not ops.nologfile:
         from logging import handlers
         import datetime
