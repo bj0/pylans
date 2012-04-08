d6e3ee02ec3a9f5033627a3b87824de6e316d250
Brian Parma <execrable@gmail.com>
2012-04-07 17:40:08 -0700

    typo

--- pylans/plogging.py
+++ pylans/plogging.py
@@ -82,8 +82,9 @@ def _new_exc_handler(exc_type, exc_value, exc_traceback):
     '''log unhandled exceptions'''
     import traceback
     logger = logging.getLogger('unhandled exception')
-    logger.always(traceback.format_exception(exc_type, exc_value, exc_traceback)
+    logger.always(traceback.format_exception(exc_type, exc_value, exc_traceback))
         
     _exc_handler(exc_type, exc_value, exc_traceback)
     
+import sys    
 _exc_handler, sys.excepthook = sys.excepthook ,_new_exc_handler
